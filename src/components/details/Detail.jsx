532c89e986a59f9f4913f0c27ed474b7abfc66e6
abhirupkumar <abhirupkumar001@gmail.com>
2024-09-11 18:13:58 +0530

    Updated Chat details with css and jsx file

--- src/components/details/Detail.jsx
+++ src/components/details/Detail.jsx
@@ -2,7 +2,50 @@ import "./detail.css"
 
 const Detail = () => {
   return (
-    <div className="detail">Detail</div>
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt=""/>
+        <h2>Abhirup Kumar</h2>
+        <p>Hi I am currently</p>
+      </div>
+      <div className="info">
+        <div className="option">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Privacy & help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetails">
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperset.com%2Fw%2Ffull%2F2%2F6%2F6%2F15300.jpg&f=1&nofb=1&ipt=67f5f58696dbbafcbdab1b66776f6b3e370eefcedc85f9071e1569e6c528a04d&ipo=images" alt="" />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon" />
+            </div>
+          </div>
+        </div>
+        <div className="option">
+          <div className="title">
+            <span>Shared Files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+        <button className="logout">Logout</button>
+      </div>
+    </div>
   )
 }
 
